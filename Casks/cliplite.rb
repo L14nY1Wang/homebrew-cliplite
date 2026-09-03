8ff34d8315f2929dd299c2f8470b6a99fab47afc
L14nY1Wang <lianyiyes00@gmail.com>
2026-09-03 23:15:37 +0800

    Add cliplite cask v0.1.0

---
+++ Casks/cliplite.rb
@@ -0,0 +1,20 @@
+cask "cliplite" do
+  arch arm: "arm64"
+
+  version "0.1.0"
+  sha256 arm: "9c1d03dad83f7ec7076bccdd2e959bd41ee1d407d9ec6ddfbd6e2b709da538bf"
+
+  url "https://github.com/L14nY1Wang/ClipLite/releases/download/v#{version}/ClipLite-#{version}.dmg"
+  name "ClipLite"
+  desc "极低内存的 macOS 截图 / 贴图 / 标注 / OCR 工具"
+  homepage "https://github.com/L14nY1Wang/ClipLite"
+
+  livecheck do
+    url :latest
+    strategy :github_latest
+  end
+
+  app "ClipLite.app"
+
+  zap trash: "~/Library/Preferences/com.lianyi.cliplite.plist"
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-cliplite
+
+Homebrew tap for [ClipLite](https://github.com/L14nY1Wang/ClipLite)。
+
+```bash
+brew tap L14nY1Wang/cliplite
+brew install --cask cliplite
+```
